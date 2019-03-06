1209a67a91b86bebcd8cbe99fa5d05e11bf63241
Bituser <bituser@bca>
2019-03-06 18:55:08 +0700

    progress home-top

--- app/components/section/home/1-top/component.js
+++ app/components/section/home/1-top/component.js
@@ -2,6 +2,22 @@ import Component from '@ember/component';
 
 export default Component.extend({
 
-  tagName: 'section'
+  tagName: 'section',
 
+  // Lesson - 1
+  // {
+  //   className: ["m-0"],
+  //   classNameBindings: ['coba:primary-bg:secondary-bg'],
+  //   coba:false,
+  //   attributeBindings: ['customHref:href'],
+
+  //   customHref: 'http://emberjs.com'
+  // }
+  // {
+  //   didInsertElement(){
+  //     this._super(...arguments)
+  //     this.$().append('-2')
+  //   }
+  // }
+  
 });
