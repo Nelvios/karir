49790e0d6425c5ec38e0fa499b3a6643583f0623
ELVIN PANTOWIDJOYO <U055585@bca>
2019-03-06 18:54:36 +0700

    navbar done

--- app/components/view/view-navbar/component.js
+++ app/components/view/view-navbar/component.js
@@ -1,4 +1,25 @@
 import Component from '@ember/component';
+import { jQ, set } from 'karir/utils/short';
 
 export default Component.extend({
+
+  tagName: 'nav',
+  classNameBindings: ['isTop:top'],
+
+  didInsertElement() {
+    this._super(...arguments);
+    const THRESHOLD = 16;
+
+    jQ(window).scroll(() => {
+
+      if(jQ(window).scrollTop() > THRESHOLD) {
+        set(this, 'isTop', false);
+      } else {
+        set(this, 'isTop', true);
+      }
+
+    });
+    jQ(window).scroll();
+  }
+
 });
