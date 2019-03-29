d12be28b8702b9251640e1fa791a3d2a2276cb04
ELVIN PANTOWIDJOYO <U055585@bca>
2019-03-29 20:26:09 +0700

    before merge

--- app/components/section/home/3-articles/component.js
+++ app/components/section/home/3-articles/component.js
@@ -2,6 +2,26 @@ import Component from '@ember/component';
 
 export default Component.extend({
 
-  tagName: 'section'
+  tagName: 'section',
+
+  articles: [
+    {
+      title: 'Nvidia is bringing ray tracing to old GPUs that can\'t tray race worth a damn',
+      date: '2019-03-19'
+    },
+    {
+      title: 'Should the United States Help Drive Global Development?',
+      date: '2018-10-18'
+    },
+    {
+      title: 'The London Olympics as Seen from the Other Side of the World',
+      date: '2018-10-05'
+    },
+    {
+      title: 'Unpatriotic History of the Second World War, by James Heartfield',
+      date: '2018-10-02'
+    }
+  ]
+
 
 });
--- app/helpers/sum.js
+++ app/helpers/sum.js
@@ -2,14 +2,15 @@ import { helper } from '@ember/component/helper';
 
 export function sum(params/*, hash*/) {
   if (params.length === 0) return null;
+  const numbers = params.map((num) => num);
 
-  switch (params.length) {
+  switch (numbers.length) {
     case 0: return null;
-    case 1: params.pushObject(1); break;
+    case 1: numbers.pushObject(1); break;
     default: break;
   }
 
-  return params.reduce((num, op) => num*1 + op*1);
+  return numbers.reduce((num, op) => num*1 + op*1);
 }
 
 export default helper(sum);
