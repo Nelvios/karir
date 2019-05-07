2cb011f010fbb618f0f299acfa1f822e90b2bb3a
Bituser <bituser@bca>
2019-05-07 17:18:10 +0700

    progress on about page and feed counters data

--- app/components/section/about/3-statistic/component.js
+++ app/components/section/about/3-statistic/component.js
@@ -1,7 +1,21 @@
 import Component from '@ember/component';
+import { jQ, get, service } from 'karir/utils/short';
+import { getProperties } from '@ember/object';
 
 export default Component.extend({
 
-  tagName:'section'
+  tagName:'section',
+  store: service(),
+
+  counters: null,
+
+  didInsertElement() {
+    this._super(...arguments);
+    const store = get(this, 'store');
+
+    store.findAll('counter').then(result => result.forEach((item) => {
+      this.set('counters', getProperties(item,'employCount'));
+    }))
+  }
 
 });
