6bce1fc24a45faff1dc3567e0d83e70d084e1757
Vito Vicnentdo <vito.vincentdo97@gmail.com>
2019-06-11 11:31:35 +0700

    Redesign Landing Page

--- app/models/thought/serializer.js
+++ app/models/thought/serializer.js
@@ -5,7 +5,7 @@ export default Serializer.extend({
   normalizeArrayResponse(store, model, payload, ...args) {
     const keys = Object.keys(payload);
     const data = payload[keys[0]];
-    const limit = 6;
+    const limit = 4;
 
     for(let i = data.length-1; i >= 0; i--) {
       let tmp, selected = Math.round(Math.random() * i);
