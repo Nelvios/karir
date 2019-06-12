93e3349eaa70e18e49c255837247e4292b8503ec
Bituser <bituser@bca>
2019-06-12 11:48:41 +0700

    change on thought

--- app/components/section/home/4-thoughts/component.js
+++ app/components/section/home/4-thoughts/component.js
@@ -12,6 +12,10 @@ export default Component.extend({
   didInsertElement() {
     this._super(...arguments);
     const store = this.get('store');
+    const slider = this.$('.slider');
+
+    slider.slider({indicators: false});
+    slider.slider('pause');
 
     store.findAll('thought').then(result => {
       set(this, 'isLoading', false);
@@ -27,6 +31,17 @@ export default Component.extend({
   willDestroyElement() {
     this._super(...arguments);
     this.$('.tooltip').tooltip('destroy');
+  },
+
+  actions: {
+    moveToPrev(){
+      this.$('.slider').slider('prev');
+      this.$('.slider').slider('pause');
+    },
+    moveToNext(){
+      this.$('.slider').slider('next');
+      this.$('.slider').slider('pause');
+    }
   }
 
 });
