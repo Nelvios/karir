87793e719a49ba9f449b854e7fc6eebe646bb856
Vito Vicnentdo <vito.vincentdo97@gmail.com>
2019-06-19 19:48:48 +0700

    change on thought and job

--- app/components/section/home/4-thoughts/component.js
+++ app/components/section/home/4-thoughts/component.js
@@ -8,18 +8,18 @@ export default Component.extend({
 
   model: null,
   isLoading: true,
+  showModel: null,
+  counter: 0,
 
   didInsertElement() {
     this._super(...arguments);
     const store = this.get('store');
-    const slider = this.$('.slider');
-
-    slider.slider({indicators: false, height: 350});
-    slider.slider('pause');
+    // const slider = this.$('.slider');
 
     store.findAll('thought').then(result => {
       set(this, 'isLoading', false);
       set(this, 'model', result);
+      set(this, 'showModel', result.get('firstObject'));
     });
   },
 
@@ -35,12 +35,37 @@ export default Component.extend({
 
   actions: {
     moveToPrev(){
-      this.$('.slider').slider('prev');
-      this.$('.slider').slider('pause');
+      const model = this.get('model');
+      let counter = this.get('counter');
+      counter -= 1;
+      if(counter < 0){
+        counter = 3;
+      }
+      this.set('showModel', model.objectAt(counter));
+      // this.$('.content').animate({opacity: 1}, 1000);
+      this.set('counter', counter);
+      // else{
+      //   this.set('showModel', model.objectAt(counter));
+      //   this.set('counter', counter);
+      // }
+      // this.set('showModel', model.objectAt(1));
+      // console.log(model.objectAt(1));
+      // const first = model.get('firstObject');
+      // console.log(first.get('id'));
+      // this.$('.slider').slider('prev');
+      // this.$('.slider').slider('pause');
     },
     moveToNext(){
-      this.$('.slider').slider('next');
-      this.$('.slider').slider('pause');
+      const model = this.get('model');
+      let counter = this.get('counter');
+      counter += 1;
+      if(counter > 3){
+        counter = 0;
+      }
+      this.set('showModel', model.objectAt(counter));
+      this.set('counter', counter);
+      // this.$('.slider').slider('next');
+      // this.$('.slider').slider('pause');
     }
   }
 
