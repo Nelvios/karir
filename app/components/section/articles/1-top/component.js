f669a4abf1449008fd2417fc29434f19bd290044
Bituser <bituser@bca>
2019-06-28 18:32:08 +0700

    progress on articles

--- app/components/section/articles/1-top/component.js
+++ app/components/section/articles/1-top/component.js
@@ -1,7 +1,28 @@
 import Component from '@ember/component';
+import { set, service } from 'karir/utils/short';
 
 export default Component.extend({
 
-  tagName: 'section'
+  tagName: 'section',
+  store: service(),
+
+  model: null,
+  showModel: null,
+  filtered: null,
+  isLoading:true,
+  counter: 0,
+
+  didInsertElement(){
+    this._super(...arguments);
+    const store = this.get('store');
+
+    store.findAll('article').then(result => {
+      let filter = result.filter((item, idx) => idx < 6);
+      set(this, 'isLoading', false);
+      set(this, 'model', result);
+      set(this, 'showModel', filter.get('firstObject'));
+      set(this, 'filtered', filter)
+    });
+  }
 
 });
