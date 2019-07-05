96666c96a17cc70cec1f042ae3c447dcf8df42bd
Bituser <bituser@bca>
2019-07-05 17:53:04 +0700

    progress on article

--- app/components/section/articles/2-content/component.js
+++ app/components/section/articles/2-content/component.js
@@ -1,7 +1,46 @@
 import Component from '@ember/component';
+import { jQ, get, set } from 'karir/utils/short';
 
 export default Component.extend({
 
-  tagName: 'section'
+  tagName: 'section',
+
+  didInsertElement(){
+    this._super(...arguments);
+
+    // const y = $(document).scrollTop();
+    // const t = $('#rows').parent().offset().top;
+    // console.log(y);
+    // console.log(t);
+
+    jQ('#toc').each(function(){
+      const y = $(document).scrollTop();
+      const t = $('#rows').parent().offset().top;
+      if (y > t){
+        console.log("masuk sini")
+        $(this).fadeIn();
+      }
+      else{
+        $(this).fadeOut();
+      }
+    });
+    // this.$('#toc').each(function(){
+    //   const y = $(document).scrollTop();
+    //   const t = $('#rows').parent().offset().top;
+    //   if (y > t){
+    //     console.log("masuk sini")
+    //     $(this).fadeIn();
+    //   }
+    //   else{
+    //     $(this).fadeOut();
+    //   }
+    // });
+  }
+
+  // actions:{
+  //   selectedUser(){
+
+  //   }
+  // }
 
 });
--- app/components/view/view-navbar/component.js
+++ app/components/view/view-navbar/component.js
@@ -10,6 +10,7 @@ export default Component.extend({
     this._super(...arguments);
     const THRESHOLD = 16;
     const body = jQ('body');
+    console.log(jQ(window).scroll());
 
     jQ(window).scroll(() => {
 
